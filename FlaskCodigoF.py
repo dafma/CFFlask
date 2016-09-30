ebdc3f16296a57d192ffa127bb1ff556bc9ebfba
dafma <mar_tp@hotmail.es>
2016-09-29 21:38:00 -0500

    ruta

--- FlaskCodigoF.py
+++ FlaskCodigoF.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!a'
+
+@app.route('/webos')
+def webos():
+    return 'webos!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
